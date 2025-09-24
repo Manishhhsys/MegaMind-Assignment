4d0ab226f4431e32db2f15b957af1658883d7280
manish076 <manishku073@gmail.com>
2025-09-24 16:48:27 +0530

    profiles route doen

---
+++ src/app/api/profiles/route.ts
@@ -0,0 +1,37 @@
+import prisma from "@/lib/prismaConfig";
+import { NextRequest, NextResponse } from "next/server";
+
+
+export  async function GET(req:NextRequest){
+    try{
+            const response=await prisma.profile.findMany({
+                select:{
+                    id:true,
+                    name:true,
+                    avatarUrl:true,
+                    gender:true,
+                    age:true,
+                    pronouns:true
+                }
+            })
+            if(response.length===0){
+                return NextResponse.json({
+                    message:"No Data Found"
+                },{
+                    status:204
+                })
+            }
+
+            return NextResponse.json({
+                data:response
+            },{
+                status:200
+            })
+    }catch(e){
+        console.log("the error.../",e)
+        return NextResponse.json({
+            message:"Internal Error"
+        },{status:500})
+    }
+
+}
