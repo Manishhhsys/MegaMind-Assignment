7f8a01090e3562e500a0c3addf7f472c9f358ab5
manish076 <manishku073@gmail.com>
2025-09-24 17:32:04 +0530

    Done With Id Based Profile Fetch

---
+++ src/app/api/profiles/[id]/route.ts
@@ -0,0 +1,44 @@
+import prisma from "@/lib/prismaConfig";
+import { NextRequest, NextResponse } from "next/server"
+
+interface paramsInteface{
+    params:{
+        id:string
+    }
+}
+
+export async function GET(req:NextRequest,{ params }:paramsInteface){
+        try{
+            const { id } = params;
+            if(!id){
+                return NextResponse.json({
+                    message:"Please Provide the User id"
+                },{status:403})
+            }
+            const response=await prisma.profile.findUnique({
+                where:{
+                    id
+                },include:{
+                    projects:true,
+                    casestudies:true
+                }
+            })
+
+            if(!response){
+                return NextResponse.json({
+                    message:"Please Enter An Valid User ID"
+                },{status:401})
+            }
+
+            return NextResponse.json({
+                data:response
+            },{status:200})
+        }catch(e:any){
+            console.log("Erorr Debug",e)
+            return NextResponse.json({
+                message:"Internal Error While Feteching the Info"
+            },{
+                status:500
+            })
+        }
+}
