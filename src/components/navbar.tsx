59de19f0cf3fb0eb5339ee4ed92f271b0e3ed888
manish076 <manishku073@gmail.com>
2025-09-26 03:19:19 +0530

    fixed Logo Issue

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,7 +5,7 @@ function NavBar() {
   return (
     <div className='w-full h-[100px] bg-white flex justify-between  items-center lg:p-15 sticky top-0 z-10 lg:gap-0 gap-10 p-8'>
         <div className=''>
-            <Image src={"/mainlogo.png"} width={247} height={64} alt='Mainlogo' className=''/>
+            <Image src={"/MainLogo.png"} width={247} height={64} alt='Mainlogo' className=''/>
         </div>
         <div className='flex lg:gap-3 md:gap-2 gap-2 '>
             <Image src={"/SideLogo1.png"} width={97} height={40} alt='prime-logo'/>
